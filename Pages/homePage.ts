b1a58c6b230e860420e42f5c10c5d8e13996f08e
Carlos Gustavo Zalazar BLV <gustavo.zalazar-x@unqork.com>
2025-04-16 23:00:31 -0400

    Initial Commit Test6

---
+++ Pages/homePage.ts
@@ -0,0 +1,3 @@
+class HomePage {
+    let userName = '#ctl00_MainContent_LoginControl1_TextBoxEmail';
+}
--- step_definitions/steps.ts
+++ step_definitions/steps.ts
@@ -1,10 +1,22 @@
 const { I } = inject();
+const mongoInstance = require("../helpers/mongoSingleton");
+const {ok} = require("assert");
 // Add in your custom step files
-
+//const {HomePage}=require("../helpers/HomePage");
 Given('I have a defined step', () => {
-  // TODO: replace with your own step
-    I.amOnPage('/');
-   // I.amOnPage('/steps');
-    
+    // TODO: replace with your own step
+    //I.amOnPage('/');
     I.wait(5);
+    I.amOnPage("/");
+    I.click('.HPHeaderLogin > a');
+    I.wait(5);
+    I.fillField('#ctl00_MainContent_LoginControl1_TextBoxEmail','test@testing65.com');
+    I.wait(1);
+    I.fillField('//input[@type=\'password\']','12345');
+    I.wait(2);
+    I.click('//input[@id=\'ctl00_MainContent_LoginControl1_ButtonLogin\']');
+    I.wait(3);
+    /*I.assert
+    I.waitForText()
+    I.waitForValue()*/
 });
